3410bf09acc725f51f0b1faa28dfe4a0263ebacb
dhlee <star7357@gmail.com>
2019-05-30 20:00:46 +0900

    abc

--- UDPPingServer.py
+++ UDPPingServer.py
@@ -11,7 +11,7 @@ import signal, sys
 DELIMITER = '\n'
 dict_recvPkts = dict()
 
-serverIP = '127.0.0.1'
+serverIP = 'nsl2.cau.ac.kr'
 serverPort = 34367
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
